9043905bf203083a585ac5ee5870db37ef317711
Hugo Lorenzo Mato <hugolma@ext.inditex.com>
2020-08-09 12:12:47 +0200

    + Gradient Descent Multivariable implemented + Some logs updated in computeCost function

--- ex1_SOLUTION/gradientDescentMulti.m
+++ ex1_SOLUTION/gradientDescentMulti.m
@@ -6,8 +6,12 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+features_number = size(X,2);
+theta_temp = theta;
 for iter = 1:num_iters
+    
+    % https://www.coursera.org/learn/machine-learning/supplement/U90DX/gradient-descent-for-linear-regression
+    fprintf('**********\nDebug purposes:\nTheta computed from gradient descent:%f,%f',theta(1),theta(2))
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -17,8 +21,15 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
+    for column = 1:features_number
+        if column == 1
+            % This way, we avoid one scalar multiplication even the result
+            % is the same
+            theta_temp(column) = theta(column) -  alpha * (1/m) * sum( (X * theta - y) );
+        end
+        theta_temp(column) = theta(column) -  alpha * (1/m) * sum( (X * theta - y) .* X(:,column) );        
+    end
+    theta = theta_temp;
 
 
 
@@ -28,9 +39,9 @@ for iter = 1:num_iters
 
 
     % ============================================================
-
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
+    fprintf('\nIteration: %d \nCost: %f\n **********', iter, J_history(iter))
 
 end
 
